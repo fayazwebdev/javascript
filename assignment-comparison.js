c369a7278506b48c450ca0ce26876bcf3b1ba91f
Fayaz Mohammad <fayazwebdev@gmail.com>
2025-10-19 11:06:11 +0530

    Assignment and Comparison

---
+++ assignment-comparison.js
@@ -0,0 +1,28 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+console.log("Try programiz.pro");
+
+let a = 5;
+let b = "5";
+
+console.log(`The value in variable a is ${a} and its datatype is "${typeof a}"`)
+console.log(`The value in variable b is ${b} and its datatype is "${typeof b}"`)
+
+
+console.log("Let compare the values of these two variables using '==' operator")
+
+if (a == b) {
+    console.log("Both the values are equal using '=='")
+} else {
+    console.log("Both values are not equal using '=='")
+}
+
+console.log("Now lets compare the values of two variables using '===' operator")
+
+if (a === b) {
+    console.log("Both values are equal using '==='")
+} else {
+    console.log("Both values are not equal using '==='")
+}
+
